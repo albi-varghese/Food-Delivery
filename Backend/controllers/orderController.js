941f22111dc000a65f34fb59abae578a5a0d4620
albi-varghese <138666731+albi-varghese@users.noreply.github.com>
2025-02-26 16:53:19 +0530

    Update orderController.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing order from frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://food-del-frontend-ro5d.onrender.com/";
 
     try {
         const newOrder = new orderModel({
